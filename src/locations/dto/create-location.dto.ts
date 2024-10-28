6be4010388d6d06f359681811609de55404645aa
beesou777 <sbeesou@gmail.com>
2024-10-28 23:22:13 +0545

    location data

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,15 +1,30 @@
-import { IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateLocationDto {
   @IsString()
+  @IsNotEmpty()
   city: string;
 
   @IsString()
+  @IsNotEmpty()
   country: string;
 
   @IsString()
+  @IsNotEmpty()
   address: string;
 
   @IsString()
+  @IsNotEmpty()
   state : string
+
+  @IsInt()
+  @IsNotEmpty()
+  lat : number
+
+  @IsInt()
+  @IsNotEmpty()
+  long : number
+
+  @IsString()
+  postalCode?: string
 }
